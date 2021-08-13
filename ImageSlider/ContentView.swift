c652de38f01a7bc0b0c818b56b7749224a85e73d
Mahdi Mahjoobi <mahdi.m@email.com>
2021-08-14 00:24:53 +0430

    Initial Commit

---
+++ ImageSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageSlider
+//
+//  Created by Mahdi Mahjoobi on 8/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImageSlider/ImageSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSliderApp.swift
+//  ImageSlider
+//
+//  Created by Mahdi Mahjoobi on 8/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
